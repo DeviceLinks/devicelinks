a0364e936831b6a44d356095f4313580a7eb8f36
Fuchuanwei <2316326961@qq.com>
2025-01-11 17:28:18 +0800

    :art: layout 配置调整

--- console-ui/src/app.tsx
+++ console-ui/src/app.tsx
@@ -1,5 +1,5 @@
 import { DEFAULT_NAME } from '@/constants';
-import { LinkOutlined, Question } from '@ant-design/icons';
+import { LinkOutlined } from '@ant-design/icons';
 import { Link } from '@umijs/max';
 const isDev = process.env.NODE_ENV === 'development';
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
@@ -45,16 +45,5 @@ export const layout = () => {
           </Link>,
         ]
       : [],
-    actionsRender: () => [
-      <Question key="doc" />,
-      <SelectLang key="SelectLang" />,
-    ],
-    avatarProps: {
-      src: initialState?.currentUser?.avatar,
-      title: <AvatarName />,
-      render: (_, avatarChildren) => {
-        return <AvatarDropdown>{avatarChildren}</AvatarDropdown>;
-      },
-    },
   };
 };
