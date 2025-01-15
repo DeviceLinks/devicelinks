27eb60dbb649657e92105001a9bfe5ac11eee7a7
hengboy <jnyuqy@gmail.com>
2025-01-15 17:16:20 +0800

    :tada: 初始化coap协议transport

---
+++ transports/transport-coap/src/main/java/cn/devicelinks/transport/coap/TransportCoapApplication.java
@@ -0,0 +1,18 @@
+package cn.devicelinks.transport.coap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CoAP协议传输服务启动类
+ *
+ * @author 恒宇少年
+ * @since 1.0
+ */
+@SpringBootApplication
+public class TransportCoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportCoapApplication.class, args);
+    }
+}
