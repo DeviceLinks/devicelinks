c427048e30942c618f5b6d825381c70967805695
hengboy <jnyuqy@gmail.com>
2025-01-15 17:16:12 +0800

    :tada: 初始化http协议transport

---
+++ transports/transport-http/src/main/java/cn/devicelinks/transport/http/TransportHttpApplication.java
@@ -0,0 +1,18 @@
+package cn.devicelinks.transport.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * HTTP协议传输服务启动类
+ *
+ * @author 恒宇少年
+ * @since 1.0
+ */
+@SpringBootApplication
+public class TransportHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportHttpApplication.class, args);
+    }
+}
