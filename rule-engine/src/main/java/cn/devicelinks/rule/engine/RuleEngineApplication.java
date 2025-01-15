f471c2cbe67a84e24e207012511a3c57c46cfb57
hengboy <jnyuqy@gmail.com>
2025-01-15 16:27:09 +0800

    :art: 将gateway模块改为rule-engine

--- rule-engine/src/main/java/cn/devicelinks/rule/engine/RuleEngineApplication.java
+++ rule-engine/src/main/java/cn/devicelinks/rule/engine/RuleEngineApplication.java
@@ -1,4 +1,4 @@
-package cn.devicelinks.gateway;
+package cn.devicelinks.rule.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0
  */
 @SpringBootApplication
-public class GatewayApplication {
+public class RuleEngineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(RuleEngineApplication.class, args);
     }
 }
