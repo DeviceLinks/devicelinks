2f081e5805ca9af1b330868ae32680ef8b78d01a
hengboy <jnyuqy@gmail.com>
2025-01-15 17:26:23 +0800

    :tada: 初始化jt808协议transport

---
+++ transports/transport-jt808/src/main/java/cn/devicelinks/transport/jt808/TransportJT808Application.java
@@ -0,0 +1,18 @@
+package cn.devicelinks.transport.jt808;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * "JT/T 808"协议传输服务启动类
+ *
+ * @author 恒宇少年
+ * @since 1.0
+ */
+@SpringBootApplication
+public class TransportJT808Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportJT808Application.class, args);
+    }
+}
