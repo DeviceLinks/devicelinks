2ef271be3a524a89ebe2a74a3d1bbb910dbf7499
hengboy <jnyuqy@gmail.com>
2025-01-22 14:21:46 +0800

    :art: 排序字段参数必须满足正则表达式才允许传递

--- console/src/main/java/cn/devicelinks/console/model/page/PageRequest.java
+++ console/src/main/java/cn/devicelinks/console/model/page/PageRequest.java
@@ -1,3 +1,20 @@
+/*
+ *   Copyright (C) 2024-2025  DeviceLinks
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package cn.devicelinks.console.model.page;
 
 import cn.devicelinks.framework.common.Constants;
@@ -6,6 +23,7 @@ import cn.devicelinks.framework.jdbc.core.definition.Column;
 import cn.devicelinks.framework.jdbc.core.page.PageQuery;
 import cn.devicelinks.framework.jdbc.core.sql.SortBy;
 import cn.devicelinks.framework.jdbc.core.sql.SortCondition;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
@@ -29,6 +47,7 @@ public class PageRequest {
     private int page;
 
     @Length(max = 50)
+    @Pattern(regexp = "^[\\p{L}0-9_-]+$", message = "排序属性格式不正确")
     private String sortProperty;
 
     @EnumValid(target = Direction.class, message = "排序顺序不允许传递非法值")
