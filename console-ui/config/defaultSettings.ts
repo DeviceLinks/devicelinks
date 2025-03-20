64e13784d03046ebc61e9dadce27e9dd5277b346
Fuchuanwei <2316326961@qq.com>
2025-03-20 11:59:17 +0800

    :art: 替换logo

--- console-ui/config/defaultSettings.ts
+++ console-ui/config/defaultSettings.ts
@@ -34,7 +34,7 @@ const Settings: ProLayoutProps & {
   colorWeak: false,
   title: 'Devicelinks',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/logo.svg',
   iconfontUrl: '',
   token: {
     // 参见ts声明，demo 见文档，通过token 修改样式
