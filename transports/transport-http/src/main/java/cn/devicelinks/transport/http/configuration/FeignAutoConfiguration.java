11096ea612c13b551c226a7c6b84c721c6b44396
恒宇少年 <jnyuqy@gmail.com>
2025-04-07 15:26:23 +0800

    :rocket: HTTP协议传输服务添加feign配置

---
+++ transports/transport-http/src/main/java/cn/devicelinks/transport/http/configuration/FeignAutoConfiguration.java
@@ -0,0 +1,94 @@
+package cn.devicelinks.transport.http.configuration;
+
+import cn.devicelinks.framework.common.Constants;
+import cn.devicelinks.framework.common.feign.CoreServiceDeviceApi;
+import cn.devicelinks.framework.common.feign.FeignConstants;
+import cn.devicelinks.framework.common.utils.HmacSignature;
+import feign.Feign;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.okhttp.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+/**
+ * Feign配置
+ *
+ * @author 恒宇少年
+ * @since 1.0
+ */
+@Configuration
+public class FeignAutoConfiguration {
+
+    private final TransportHttpProperties httpTransportProperties;
+
+    public FeignAutoConfiguration(TransportHttpProperties httpTransportProperties) {
+        this.httpTransportProperties = httpTransportProperties;
+    }
+
+    @Bean
+    public CoreServiceDeviceApi coreServiceDeviceApi() {
+        TransportHttpProperties.CoreServiceFeignConfig coreServiceFeignConfig = httpTransportProperties.getCoreServiceAccess();
+        return Feign.builder()
+                .client(new OkHttpClient())
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .requestInterceptor(signRequestInterceptor())
+                .target(CoreServiceDeviceApi.class, coreServiceFeignConfig.getUri());
+    }
+
+    @Bean
+    public RequestInterceptor signRequestInterceptor() {
+        return new SignRequestInterceptor(httpTransportProperties.getCoreServiceAccess());
+    }
+
+    /**
+     * The Sign RequestInterceptor
+     */
+    private static class SignRequestInterceptor implements RequestInterceptor {
+        private final TransportHttpProperties.CoreServiceFeignConfig coreServiceFeignConfig;
+
+        public SignRequestInterceptor(TransportHttpProperties.CoreServiceFeignConfig coreServiceFeignConfig) {
+            this.coreServiceFeignConfig = coreServiceFeignConfig;
+        }
+
+        @Override
+        public void apply(RequestTemplate requestTemplate) {
+            String timestamp = String.valueOf(System.currentTimeMillis());
+            requestTemplate.header(FeignConstants.API_KEY_HEADER_NAME, coreServiceFeignConfig.getApiKey());
+            requestTemplate.header(FeignConstants.API_TIMESTAMP_HEADER_NAME, timestamp);
+            requestTemplate.header(FeignConstants.API_SIGN_HEADER_NAME,
+                    this.sign(coreServiceFeignConfig.getApiSecret(), timestamp, requestTemplate));
+        }
+
+        private String sign(String secret, String timestamp, RequestTemplate requestTemplate) {
+            String queryString = this.getQueryString(requestTemplate);
+            String bodyString = this.getBodyString(requestTemplate);
+            // raw string
+            // @formatter:off
+            String raw = (!ObjectUtils.isEmpty(queryString) ? queryString : Constants.EMPTY_STRING) +
+                    (!ObjectUtils.isEmpty(bodyString) ? bodyString : Constants.EMPTY_STRING) +
+                    timestamp;
+            // @formatter:on
+            // generate hex sign
+            return HmacSignature.hmacSha256(secret).toHex(raw);
+        }
+
+        private String getQueryString(RequestTemplate template) {
+            return template.queries().entrySet().stream()
+                    .flatMap(e -> e.getValue().stream().map(v -> e.getKey() + "=" + v))
+                    .collect(Collectors.joining("&"));
+        }
+
+        private String getBodyString(RequestTemplate template) {
+            byte[] body = template.body();
+            return body != null ? new String(body, StandardCharsets.UTF_8) : "";
+        }
+    }
+}
