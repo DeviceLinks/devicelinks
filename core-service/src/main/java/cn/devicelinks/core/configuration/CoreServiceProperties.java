7a361c2406ed151b3c6bfe74839e836258b4f2bb
恒宇少年 <jnyuqy@gmail.com>
2025-04-07 10:35:26 +0800

    :memo: 核心服务添加内部通信安全配置

---
+++ core-service/src/main/java/cn/devicelinks/core/configuration/CoreServiceProperties.java
@@ -0,0 +1,30 @@
+package cn.devicelinks.core.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static cn.devicelinks.core.configuration.CoreServiceProperties.DEVICELINKS_CORE_SERVICE_PREFIX;
+
+/**
+ * 核心服务属性配置
+ *
+ * @author 恒宇少年
+ * @since 1.0
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = DEVICELINKS_CORE_SERVICE_PREFIX)
+public class CoreServiceProperties {
+    public static final String DEVICELINKS_CORE_SERVICE_PREFIX = "devicelinks.core";
+
+    private Map<String, InternalServiceSecurity> security;
+
+    @Data
+    public static class InternalServiceSecurity {
+        private String apiKey;
+        private String apiSecret;
+    }
+}
