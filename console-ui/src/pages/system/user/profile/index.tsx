e9495732ce1ca07ac3569516680823d41d47c09c
LvYan <ly18363403098@163.com>
2025-04-24 09:34:03 +0800

    :art: 用户详情界面优化

--- console-ui/src/pages/system/user/profile/index.tsx
+++ console-ui/src/pages/system/user/profile/index.tsx
@@ -1,12 +1,26 @@
 import UserInfo from '@/pages/system/user/profile/modules/UserInfo';
-import { getApiUserUserId, postApiUserUserId } from '@/services/device-links-console-ui/user';
+import {
+  deleteApiUserUserId,
+  getApiUserUserId,
+  postApiUserUserId,
+} from '@/services/device-links-console-ui/user';
 import { useSearchParams } from '@@/exports';
+import {
+  CheckCircleOutlined,
+  DeleteOutlined,
+  DisconnectOutlined,
+  DownOutlined,
+  RollbackOutlined,
+  SendOutlined,
+  StopOutlined,
+} from '@ant-design/icons';
 import { PageContainer, ProSkeleton } from '@ant-design/pro-components';
 import { history } from '@umijs/max';
-import { message, Tabs } from 'antd';
+import { Button, Dropdown, MenuProps, message, Modal, Space, Tabs, Tag, Typography } from 'antd';
 import React from 'react';
 
 const UserProfile = () => {
+  const { Paragraph } = Typography;
   const [user, setUser] = React.useState<API.User>();
   const [loading, setLoading] = React.useState(false);
   const [params] = useSearchParams();
@@ -22,6 +36,57 @@ const UserProfile = () => {
       setUser(data);
     } finally {
       setLoading(false);
+    }
+  };
+  const menuItems: MenuProps['items'] = [
+    user?.enabled
+      ? {
+          key: 'enabled',
+          icon: <CheckCircleOutlined />,
+          label: '启用用户',
+        }
+      : {
+          key: 'disabled',
+          icon: <StopOutlined />,
+          label: '禁用用户',
+        },
+    {
+      key: 'delete',
+      icon: <DeleteOutlined />,
+      label: '删除用户',
+    },
+    {
+      key: 'forceOutline',
+      icon: <DisconnectOutlined />,
+      label: '强制下线',
+    },
+
+    {
+      key: 'resetPwd',
+      icon: <RollbackOutlined />,
+      label: '重置密码',
+      disabled: !user?.email,
+    },
+    {
+      key: 'sendResetPwdLink',
+      icon: <SendOutlined />,
+      label: '发送用户重置密码链接',
+      disabled: !user?.email,
+    },
+  ];
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    if (e.key === 'delete') {
+      Modal.confirm({
+        title: '提示',
+        content: '确定要删除该用户吗？',
+        okText: '确定',
+        cancelText: '取消',
+        onOk: async () => {
+          await deleteApiUserUserId({
+            userId: user?.id,
+          } as API.deleteApiUserUserIdParams);
+        },
+      });
     }
   };
   React.useEffect(() => {
@@ -58,9 +123,32 @@ const UserProfile = () => {
       loading={loading}
       ghost
       header={{
-        title: user?.name,
-        breadcrumb: {},
+        title: `用户名称：${user?.name}`,
       }}
+      tags={
+        user && (
+          <Tag color={user.enabled ? 'success' : 'error'}>{user.enabled ? '启用中' : '禁用中'}</Tag>
+        )
+      }
+      content={
+        <Paragraph
+          copyable={{
+            text: `${user?.id}`,
+          }}
+        >
+          ID：{user?.id || '-'}
+        </Paragraph>
+      }
+      extra={
+        <Dropdown menu={{ items: menuItems, onClick: handleMenuClick }}>
+          <Button type={'primary'}>
+            <Space>
+              更多
+              <DownOutlined />
+            </Space>
+          </Button>
+        </Dropdown>
+      }
     >
       <Tabs defaultActiveKey="userInfo" items={tabItems} />
     </PageContainer>
